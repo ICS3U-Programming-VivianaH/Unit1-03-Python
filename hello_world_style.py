3aab85e680bb08c702785e37b4d66f2423c6ba09
Viviana Hurtado <viviana.hurtado-antiporta@stu.ocsb.ca>
2025-02-18 03:27:37 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,9 @@
+#Created by: Viviana Hurtado
+#Date: feb 14 2025
+#This programs displays hello world with proper style
+def main ():
+    print("hello, world! with proper style!")
+    print("I am testing my function here")
+    
+if __name__=="__main__":
+        main()
